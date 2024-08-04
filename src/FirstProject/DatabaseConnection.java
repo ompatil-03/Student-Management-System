663820bc64374be3ea62183b3cc18993dadcc0e6
Mech-Boy <om294729@gmail.com>
2024-08-04 13:14:17 +0530

    "Files Added"

--- src/FirstProject/DatabaseConnection.java
+++ src/FirstProject/DatabaseConnection.java
@@ -1,21 +1,19 @@
 package FirstProject;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/database_name";
     private static final String USER = "username";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "Password";
 
     public static Connection getConnection() {
-        Connection connection = null;
+        Connection con = null;
         try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        return connection;
+        return con;
     }
 }
